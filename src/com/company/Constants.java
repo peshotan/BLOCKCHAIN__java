4638f30acd488fe1701917099909d35096876191
peterirani <peshotan_irani@sfu.ca>
2021-01-25 16:07:15 -0800

    add(SHA256Helper, Constants) : add classes for SHA256 and Constants

---
+++ src/com/company/Constants.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Constants {
+
+    private Constants(){
+        // this class cannot be instantiated
+    }
+
+    public static final int DIFFICULT = 5;
+    public static final double MINER_REWARD = 10;
+    public static final String GENESIS_PREV_HASH = "0000000000";
+
+}
---
+++ src/com/company/SHA256Helper.java
@@ -0,0 +1,45 @@
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+
+public class SHA256Helper {
+
+    public static String generateHash(String data){
+        String hexaString;
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hashByteArray = digest.digest(data.getBytes(StandardCharsets.UTF_8));
+            hexaString = bytesToHash(hashByteArray);
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return hexaString;
+    }
+
+    public static String bytesToHash(byte[] byteArray){
+        StringBuffer hexStringBuilder = new StringBuffer();
+
+        for (int i = 0; i < byteArray.length; ++i){
+            String hexa = Integer.toHexString(0xff & byteArray[i]);
+            if (hexa.length() == 1) hexStringBuilder.append('0');
+            hexStringBuilder.append(hexa);
+        }
+        return hexStringBuilder.toString();
+    }
+
+    public static String mergeHash(String data1, String data2){
+        String concatenatedString = data1 + data2;
+        String hexaString;
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hashByteArray = digest.digest(concatenatedString.getBytes(StandardCharsets.UTF_8));
+            hexaString = bytesToHash(hashByteArray);
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return hexaString;
+    }
+
+}
